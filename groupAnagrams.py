98a0a77a825c44c3870fa95b21ac6078cf981202
Massad Raza <massad.raza@hotmail.com>
2025-11-26 00:03:30 -0500

    Solved inefficient solution groupAnagrams

---
+++ groupAnagrams.py
@@ -0,0 +1,18 @@
+class Solution:
+
+    # Created by Massad Raza 
+    def groupAnagrams(self, strs: List[str]) -> List[List[str]]:
+
+        # Inefficient Solution (uses sorting)
+        # using a defaultdict() automatically assigns a key of whatever you suggested
+        strs2 = defaultdict(list)
+
+        # Using sorted(S) returns a list of each individual character
+        # Must join using join() function with the character associated
+
+        for s in strs:
+            stringSorted = ''.join(sorted(s))
+            strs2[stringSorted].append(s)
+        return list(strs2.values())
+
+          
