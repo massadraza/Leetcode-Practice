8b80aaa55fe1e272ed32f7a99407c71526b0eea7
Massad Raza <massad.raza@hotmail.com>
2025-12-22 20:22:35 -0500

    Efficient Group Anagrams Soln.

--- groupAnagrams.py
+++ groupAnagrams.py
@@ -27,4 +27,16 @@ class Solution:
             result.append(temp)
 
         return result    
-        """            
+        """ 
+
+        res = defaultdict(list)
+
+        for s in strs:
+            count = [0] * 26
+
+            for c in s:
+                count[ord(c) - ord("a")] += 1
+
+            res[tuple(count)].append(s)
+        
+        return list(res.values())           
