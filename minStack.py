a5385487cba13157fbd74de16eb49fd3d2f90d9c
Massad Raza <massad.raza@hotmail.com>
2025-12-26 15:06:39 -0500

    Efficient soln. of minStack

--- minStack.py
+++ minStack.py
@@ -1,5 +1,5 @@
 class MinStack:
-
+    """
     def __init__(self):
         self.items = []
         self.min = float("infinity")
@@ -35,7 +35,35 @@ class MinStack:
             return self.min
         else:
             return None
+    """    
+
+    def __init__(self):
+        self.stack = []
+        self.minStack = []
+
+    def push(self, val: int) -> None:
+        self.stack.append(val)
+        val = min(val, self.minStack[-1] if self.minStack else val)
+        self.minStack.append(val)
         
+    def pop(self) -> None:
+        self.stack.pop()
+        self.minStack.pop()
+        
+    def top(self) -> int:
+        return self.stack[-1]
+
+    def getMin(self) -> int:
+        return self.minStack[-1]
+      
+        
+
+# Your MinStack object will be instantiated and called as such:
+# obj = MinStack()
+# obj.push(val)
+# obj.pop()
+# param_3 = obj.top()
+# param_4 = obj.getMin()
 
 # Your MinStack object will be instantiated and called as such:
 # obj = MinStack()
